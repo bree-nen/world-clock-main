ac725f76f8f64d1d30f1c7404e14fc27a09c7a60
bree-nen <99203626+bree-nen@users.noreply.github.com>
2025-09-15 19:47:17 +0200

    Added the current location

--- javascript/index.js
+++ javascript/index.js
@@ -36,6 +36,10 @@ setInterval(updateTime, 1);
 function updateCity(event) {
   let cityTimeZone = event.target.value;
 
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
+
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
 
   let cityTime = moment().tz(cityTimeZone);
